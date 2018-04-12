abefa047cb17c2cf3f3aba25336e4dc794261b8b
KorayKaya <korre.remix@gmail.com>
2018-04-13 01:07:31 +0200

    jquery tried

--- popup.js
+++ popup.js
@@ -15,13 +15,31 @@ function getTabid() {
 }
 
 var tabid;
+var apiKey = AIzaSyBzLH4gRgoGJu2hK9ALogIIvRDs_4v7Fec;
 
 function writeOutQueue() {
     getTabid();
+    queueText.innerHTML="";
+    alert(0);
     chrome.storage.local.get({
         'queue': []
     }, function(result) {
-        queueText.innerHTML = result.queue.join("<br/><br/>");
+      alert(1);
+        for (var i = 0; i < result.queue.length; i++) {
+          alert(1.5);
+          var videoId = result.queue[i].slice((result.queue[i].indexOf("?v=")+3),(result.queue[i].indexOf("?v=")+14));
+            $.ajax({
+                url: "https://www.googleapis.com/youtube/v3/videos?id=" + videoId + "&key=" + apiKey + "&fields=items(snippet(title))&part=snippet",
+                dataType: "jsonp",
+                success: function(data) {
+                    alert(data.items[0].snippet.title);
+                    queueText.innerHTML +=data.items[0].snippet.title;
+                },
+                error: function(jqXHR, textStatus, errorThrown) {
+                    alert(textStatus, +' | ' + errorThrown);
+                }
+            });
+        }
     });
 }
 
